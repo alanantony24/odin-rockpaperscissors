14dff2a6127f9c3832397f043edb933ab3f3a94d
Alan Antony James <s10207327@connect.np.edu.sg>
2022-08-31 23:27:58 +0800

    add function to randomly choose an option

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Hello World!")
+function getComputerChoice () {
+    const choiceArray = ['Rock', 'Paper', 'Scissors'];
+    var randomChoice = Math.floor(Math.random() * choiceArray.length);
+    return choiceArray[randomChoice];
+}
+
+console.log("Computer : " + getComputerChoice());
