c659630124b8732520aca9fcca95232a8455d8ee
Alan Antony James <s10207327@connect.np.edu.sg>
2022-09-17 22:45:44 +0800

    fix array items casing issue

--- app.js
+++ app.js
@@ -12,44 +12,56 @@ const result = {
 
 function playOneRound(playerSelection, computerSelection) {
 
-    const choiceArray = ['rock', 'paper', 'scissors'];
+    const choiceArray = ['Rock', 'Paper', 'Scissors'];
 
     if (playerSelection == choiceArray[0] && computerSelection == choiceArray[1]) {
         result.computerScore += 1;
         result.resultString = "You Lost! Paper beats Rock";
+        console.log(result);
     }
     else if (playerSelection == choiceArray[0] && computerSelection == choiceArray[2]) {
         result.playerScore += 1;
         result.resultString = "You Win! Rock beats Scissors";
+        console.log(result);
     }
     else if (playerSelection == choiceArray[1] && computerSelection == choiceArray[0]) {
         result.playerScore += 1;
         result.resultString = "You Win! Paper beats Rock";
+        console.log(result);
     }
     else if (playerSelection == choiceArray[1] && computerSelection == choiceArray[2]) {
         result.computerScore += 1;
         result.resultString = "You Lost! Scissors beats Paper";
+        console.log(result);
     }
     else if (playerSelection == choiceArray[2] && computerSelection == choiceArray[0]) {
         result.computerScore += 1;
         result.resultString = "You Lost! Rock beats Scissors";
+        console.log(result);
     }
     else if (playerSelection == choiceArray[2] && computerSelection == choiceArray[1]) {
         result.playerScore += 1;
         result.resultString = "You Win! Scissors beats Paper";
+        console.log(result);
     }
     else if (playerSelection == computerSelection) {
+        result.playerScore += 0;
+        result.computerScore += 0;
         result.resultString = "TIE";
+        console.log(result);
     }
 
-    displaySelections(playerSelection, computerSelection);
+    displayResult(playerSelection, computerSelection, result);
 
     return result;
 }
 
-function displaySelections(playerSelection, computerSelection) {
+function displayResult(playerSelection, computerSelection, result) {
     document.getElementById('playerSelection').textContent =  "Player Selection : " + playerSelection;
     document.getElementById('computerSelection').textContent = "Computer Selection : " + computerSelection;
+    document.getElementById('playerScore').textContent = "Player Score : " + result.playerScore;
+    document.getElementById('computerScore').textContent = "Computer Score : " + result.computerScore;
+    document.getElementById('result').textContent = "Result : " + result.resultString
 }
 
 
